c91be2d9cdeebe66df228ed49bedd0cd7020c159
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-06-14 19:46:51 +0000

    Sun, Jun 14, 2026, 3:46 PM -04:00

--- src/oneusecomp/ModCards.jsx
+++ src/oneusecomp/ModCards.jsx
@@ -6,17 +6,17 @@ function ModCards(){
 
     return(
         <div className="cards">
+             <Card
+            name="Muslimdiscs"
+            description="Use the Jukebox for more Islamic audios!--Adds Islamic discs to Minecraft."
+            />
             <Card
             name="Potassium Water"
             description="Potassium + Water = BOOM! Explosive Water!"
             />
             <Card
             name="Tutorial Structures"
-            description="Teach the player without Documentation or Wikia!"
-            />
-            <Card
-            name="Muslimdiscs"
-            description=""
+            description="Teach the player without the need of documentation or wikia!"
             />
             <Card
             name="Potassium Water"
