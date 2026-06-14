67d7b8415d44495bd648fd84c413a99b9ae36484
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-06-14 20:25:06 +0000

    Sun, Jun 14, 2026, 4:25 PM -04:00

--- src/reusablecomponenets/Card.jsx
+++ src/reusablecomponenets/Card.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import defaultimg from 'muslimdiscs-modpage/src/assets/pack3.png'
+import defaultimg from '/src/assets/pack3.png'
 
 function Card(props){
 
@@ -14,7 +14,7 @@ function Card(props){
 )
 }
 Card.defaultProps = {
-img: "",
+//img: defaultImg,
 alt: "TEST",
 name: "WIP",
 desc: "WIP",
