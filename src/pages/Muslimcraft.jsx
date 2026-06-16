30c87e280c915f41d1ff69da6fab262a0c2de7fd
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-06-16 05:25:00 +0000

    Tue, Jun 16, 2026, 1:25 AM -04:00

--- src/pages/Muslimcraft.jsx
+++ src/pages/Muslimcraft.jsx
@@ -5,8 +5,252 @@ function Muslimcraft(){
     return(
     <>
     <div>
-     <h1> Muslimcraft
+     <h1> Muslimcraft </h1>
 
+Makes Minecraft clearly Halal, removing any doubt that it would be Haram.
+
+This includes changing things like Pork to be Truffles (Fun fact, Truffles are a fungus that pigs can actually sniff for in real life!), and changing the Totem of Undying to, in the exact wording, a "Super High Quality Defibrillator."
+
+It SHOULD have removed Music entirely, with some of the music being replaced with Nasheeds, and the Jukebox Discs having Quran instead.
+
+As well as this, it changes some paintings to be that of Mosques, instead of what they were before.
+
+Albeit, it doesn't, unlike the full version, remove the faces from all of the webs, items, and effects (where they have it).
+
+For a more Exhaustive list, I haven't finished the website yet so this is a WIP, will be finished inshaAllah.
+
+
+## Questions/Suggestions/Problems
+
+Make an [issue](https://github.com/friedegg796/Muslimcraft-Lite/issues), I'll check it inshaAllah.
+
+### (Insert thing here) isn't Haram!
+
+Just because the pack changes it doesn't mean nessecarily it is Haram, but it may be something that is viewed as Haram (Or may have been changed to fit other changes).
+
+There are also differing opinions based on what would be considered Haram or not, I'm just trying to emcompass all of them.
+
+### Could I put your Resourcepack into MY modpack?
+Read the [License](https://github.com/friedegg796/Muslimcraft-Lite?tab=License-1-ov-file#)
+
+If on Modrinth, read their TOS as well.
+
+### How do I install...
+
+On Modrinth you install the zip and then put it in your Resourcepacks folder of your Minecraft Instance.
+
+On [Github](https://github.com/friedegg796/Muslimcraft-Lite?tab=readme-ov-file) you install the zip, unzip it, and then do the above (Unreleased versions).
+
+Or look for the named zip file in [releases](https://github.com/friedegg796/Muslimcraft-Lite/releases).
+
+
+## Credits
+
+### Textures
+<details>
+<summary>Textures</summary>
+
+In /assets/minecraft/textures/painting/
+
+For burning_skull.png:
+Nikeman916 in the Public Domain, Masjid Al-Aqsa picture.
+
+For pond.png:
+omar8000@gmail.com Gave permission.
+
+For skeleton.png:
+Jahfarov Muhammadnur in the Public Domain, Masjid-al-Haram
+
+For wither.png:
+Bdx in the Public Domain, İskenderun Nihal Atakaş Mosque
+
+for skull_and_roses.png:
+Keira.alfatih in the Public Domain, Masjid Apung Pagatan
+
+for humble.png:
+ItsAsyl in the Public Domain,The Grand Mosque named after Seyitjan-Qari Esjanuly
+
+owlemons.png:
+Spear in the Public Domain, Salama Mosque at Basateen
+
+stage.png:
+Karelj in the Public Domain, Abd Al-Mun'im Riyad Mosque
+
+creebet.png:
+Erik Albers in the Public Domain, Dome of Jamh Mosque, Yazd, Iran
+
+backyard.png:
+Rayhana Alhour in the Public Domain, مسجد قبة الصخرة في القدس
+
+bust.png:
+Plugwash~commonswiki in the Public Domain, Banda Aceh's Grand Mosque, Indonesia
+
+cavebird.png:
+
+Grenavitar in the Public Domain, Central London Mosque in London
+
+tides.png and pairie_ride:
+Houss 2020 in the Public Domain, Great Mosque of Khniss
+
+wanderer.png:
+Ettounsi21 in the Public Domain, El RAHMEN Mosque - Djerba
+
+sunflowers.png:
+
+Deepak-nsk in the Public Domain, Kapitan Keling Mosque at night
+
+sea.png:
+
+Ettounsi21 in the Public Domain, IBN ELABBAS Mosque Wudu room
+
+unpacked.png:
+
+SerChevalerie in the Public Domain, Masjid-e-Noor, Zuarinagar
+
+pointer.png:
+
+Dokumentasi Cirebon in the Public Domain, Masjid Agung Sumber Cirebon
+
+donkey_kong.png:
+
+TheHadiRahim in the Public Domain, Wide shot of the Green Dome at The Prophet (Sallahu Alayi Wassalam)'s Mosque (Al Masjid-e-Nabawi)
+
+fern.png:
+
+Houss 2020 in the Public Domain, Mosquée CASTILIA
+
+orb.png:
+
+Houss 2020 in the Public Domain, Mosquée EL FATH - Tozeur
+
+bouqet.png:
+
+Ettounsi21 in the Public Domain, ELKATEB Mosqie - Mahboubin Djerba
+
+cotan.png:
+
+Houss 2020 in the Public Domain, Mosquée Ibn Abass-Maammer - Djerba Midoun
+
+endboss.png:
+
+Houss 2020 in the Public Domain, AL TAWFIQ MOSQUE (Monastir)
+
+graham.png:
+
+Houss 2020 in the Public Domain, AL IKHLAS MOSQUE
+
+lowmist.png
+
+Ettounsi21 in the Public Domain, OULED HLEL Mosque
+
+changing.png
+
+Ettounsi21 in the Public Domain, KHALED IBN EL WALID Mosque
+
+passage.png
+
+Houss 2020 in the Public Domain, Al-Rahma Mosque (Sidi Ali Ben Aoun)
+
+fighters.png:
+
+Mirlim in the Public Domain, Masjid Kampung Bersia Lama
+
+match.png:
+
+Houss 2020 in the Public Domain, Mosquée TAQUA ALLAH
+
+void.png
+
+Addjimedu in the Public Domain, ILISAN CENTRAL MOSQUE
+
+baroque.png:
+
+Michael in the Public Domain, Pangkor Island Mosque
+
+pool.png
+
+Mostafameraji in the Public Domain, Mosque Soleyman
+
+courbet.png
+
+Addjimedu in the Public Domain, ITAMAPAKO CENTRAL MOSQUE
+
+sunset.png
+
+Houss 2020 in the Public Domain, AL NOUR MOSQUE
+
+kebab.png
+Houss 2020 in the Public Domain, Mosquée Sidi Abdelkader Gabés
+
+aztec.png
+Houss 2020 in the Public Domain, Mosquée Hadher Bach
+
+alban.png:
+Emin Tarahija in the Public Domain, Ensar džamija
+
+aztec2.png:
+
+Houss 2020 in the Public Domain, Mosquée TAQUA ALLAH- Tozeur
+
+bomb.png:
+Houss 2020 in the Public Domain, Mosquée Hadher Bach interior
+
+plant.png:
+Houss 2020 in the Public Domain, Mosquée Al Jadid - Oualegh Djerba
+
+wasteland.png:
+
+Houss 2020 in the Public Domain, Mosquée Al Rahmen bookshelf - Midoun
+
+dennis.png:
+
+DayakSibiriak in the Public Domain, Keramat Kuno mosque, Kampung Kajanan, Singaraja, Bali (estbl. c. 1645).
+
+
+For finding.png and pigscene.png:
+Public Domain
+
+</details>
+
+<details>
+<summary> Audio </summary>
+
+
+Everything in assets/minecraft/sounds/music/game/creative:
+Music Provided By NoCopyrightNasheeds
+Title: Dramatic & Calming Background Nasheed ┇ Vocals Only [No Copyright Nasheeds]
+Watch:[    • Dramatic & Calming Background Nasheed ┇ Vo...  ](https://www.youtube.com/watch?v=hlK3dqsJrEs)
+
+All files (NOT FOLDERS) in assets/minecraft/sounds/music/game except yakusoku and piano2:
+Music Provided By NoCopyrightNasheeds
+Title: Spiritual & Emotive Background Nasheed┇Vocals Only [No Copyright Nasheeds]
+Watch: [   • Spiritual & Emotive Background Nasheed┇Voc... ](https://www.youtube.com/watch?v=ADqC_b7pFFs)
+
+Don't ask me why it says to put music, I just do what I'm told.
+
+Everything in assets/minecraft/sounds/music/menu except mutation, menu2, beginning_2, and floating_trees:
+
+YouTube CC-BY 4.0  [ Creative Commons Attribution 4.0 International ](https://creativecommons.org/licenses/by/4.0/deed.en)
+
+https://www.youtube.com/watch?v=UBW9oWc1FNU
+
+Video was made into an OGG audio file, and was shortened to about 17 Minutes and 7 seconds.
+
+Careful with the above video's channel, this specific video was fine but I fear there may be not-so-good content on there.
+
+Audio for the Discs:
+https://www.mp3quran.net/eng with it itsself saying "Copyrights: All rights are available to everyone, and we allow any visitor or developer to copy any material or use any link on the websites"
+
+</details>
+
+#### Disclaimer
+
+
+I do NOT CONDONE the credited, I only took what was benefical from them; I am not responsible for what they do, but I do appreciate the benefical things they brought, May Allah guide us and them and grant us all Jannah tul Firdaws al `Ala.
+
+And although some images may be taken from the Public Domain, their modified versions are not in the Public Domain.
+
+NOT AN OFFICIAL MINECRAFT PRODUCT. NOT APPROVED BY OR ASSOCIATED WITH MOJANG OR MICROSOFT.
     </div>
     </>
     )
