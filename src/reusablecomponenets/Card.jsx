2fc194951efe9c46b1ee8a1835c7d238fe34bd87
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-06-17 22:18:09 +0000

    Wed, Jun 17, 2026, 6:18 PM -04:00

--- src/reusablecomponenets/Card.jsx
+++ src/reusablecomponenets/Card.jsx
@@ -4,13 +4,15 @@ import defaultimg from '/src/assets/pack3.png'
 function Card({img = {}, link = {} , alt = "WIP", name = 0, desc = "none" }){
 
    return(
-  <div className="card">
     <Link to={link}>
+  <div className="card">
+
         <img src={img} alt={alt} width="200" />
         <h2> {name} </h2>
         <p> {desc} </p>
-    </Link>
+
    </div>
+   </Link>
 )
 }
 export default Card
