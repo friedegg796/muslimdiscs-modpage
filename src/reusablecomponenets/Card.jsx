2b0b3a495e9917046b0403a0850d72ac6ba68507
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-06-17 22:13:29 +0000

    Wed, Jun 17, 2026, 6:13 PM -04:00

--- src/reusablecomponenets/Card.jsx
+++ src/reusablecomponenets/Card.jsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom'
 import defaultimg from '/src/assets/pack3.png'
 
-function Card(props){
+function Card({img = {}, link = {} , alt = "WIP", name = 0, desc = "none" }){
 
    return(
   <div className="card">
-    <Link to={props.link}>
-        <img src={props.img} alt={props.alt} width="200" />
-        <h2> {props.name} </h2>
-        <p> {props.desc} </p>
+    <Link to={link}>
+        <img src={img} alt={alt} width="200" />
+        <h2> {name} </h2>
+        <p> {desc} </p>
     </Link>
    </div>
 )
