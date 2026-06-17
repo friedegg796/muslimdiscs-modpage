10b557a767556e2ce8f0b15daa7d5254b8d10445
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-06-17 22:53:06 +0000

    Wed, Jun 17, 2026, 6:53 PM -04:00

--- src/pages/Tutorialstructures.jsx
+++ src/pages/Tutorialstructures.jsx
@@ -10,6 +10,28 @@ function Tutorialstructures(){
 
 <p>Adds in structures that teach the player certain mechanics.</p>
 
+
+<br/>
+
+<hr/>
+
+<h2> Questions/Suggestions/Problems </h2>
+
+Make an [issue](https://github.com/friedegg796/Muslimcraft-Lite/issues), I'll check it inshaAllah.
+
+### Could I put your Mod nto MY modpack?
+Read the [License](https://github.com/friedegg796/Muslimcraft-Lite?tab=License-1-ov-file#)
+
+If on Modrinth, read their TOS as well.
+
+<h3> How do I install...</h3>
+
+On here you go to <b> Downloads </b>, install the zip and then put it in your Resourcepacks folder of your Minecraft Instance.
+
+On [Github](https://github.com/friedegg796/Muslimcraft-Lite?tab=readme-ov-file) you install the zip, unzip it, and then do the above (Unreleased/Latest versions).
+
+Or look for the named zip file in [releases](https://github.com/friedegg796/Muslimcraft-Lite/releases).
+
     </div>
     </>
     )
