5977b88f8a79aa19cd646a6aae9d0f9ea12d5fa9
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-06-17 23:02:34 +0000

    Wed, Jun 17, 2026, 7:02 PM -04:00

--- src/pages/Tutorialstructures.jsx
+++ src/pages/Tutorialstructures.jsx
@@ -17,19 +17,19 @@ function Tutorialstructures(){
 
 <h2> Questions/Suggestions/Problems </h2>
 
-Make an [issue](https://github.com/friedegg796/TutorialStructures/issues), I'll check it inshaAllah.
+<p> Make an <a href="https://github.com/friedegg796/TutorialStructures/issues"> Issue </a>, I'll check it inshaAllah. </p>
 
 <h3> Could I put it into MY modpack/Use it in my video? </h3>
 
-Read the [License](https://github.com/friedegg796/TutorialStructures?tab=License-1-ov-file)
+ <p>Read the [License](https://github.com/friedegg796/TutorialStructures?tab=License-1-ov-file) </p>
 
-For those on Modrinth please read it's TOS before incorporating it into your modpack.
+ <p>For those on Modrinth please read it's TOS before incorporating it into your modpack.</p>
 
 <h3> How do I install...</h3>
 
 On here you go to <b> Downloads </b>, install the zip and then put it in your Resourcepacks folder of your Minecraft Instance.
 
-On [Github](https://github.com/friedegg796/Muslimcraft-Lite?tab=readme-ov-file) you install the zip, unzip it, and then do the above (Unreleased/Latest versions).
+On <a href="https://github.com/friedegg796/TutorialStructures?tab=readme-ov-file> Github </a> you install the zip, unzip it, and then do the above (Unreleased/Latest versions).
 
 Or look for the named zip file in [releases](https://github.com/friedegg796/Muslimcraft-Lite/releases).
 
