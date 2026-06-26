517a4a8a4aa6a26e3e31377b74a9176dc59ba41c
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-06-26 11:44:31 +0000

    Fri, Jun 26, 2026, 7:44 AM -04:00

--- src/reusablecomponenets/Footer.jsx
+++ src/reusablecomponenets/Footer.jsx
@@ -4,9 +4,9 @@ function Footer() {
 
     return(
         <>
-        <hr/>
-        <br/>
         <footer>
+        <br/>
+        <hr/>
        <p> NOT AN OFFICIAL MINECRAFT PRODUCT. NOT APPROVED BY OR ASSOCIATED WITH MOJANG OR MICROSOFT.</p>
 
        <p> Copyright © 2026 Hasnain Alahi - All rights reserved  </p>
