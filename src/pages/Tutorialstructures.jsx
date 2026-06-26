2b23678a9ff052162abfa7db0fbc265394a185c9
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-06-26 06:31:55 +0000

    Fri, Jun 26, 2026, 2:31 AM -04:00

--- src/pages/Tutorialstructures.jsx
+++ src/pages/Tutorialstructures.jsx
@@ -27,11 +27,15 @@ function Tutorialstructures(){
 
 <h3> How do I install...</h3>
 
-On here you go to <b> Downloads </b>, install the zip and then put it in your Resourcepacks folder of your Minecraft Instance.
+<p>On here you go to <b> Downloads </b>,
+install the zip and
+then put it in your Resourcepacks folder of your Minecraft Instance.</p>
 
-On <a href="https://github.com/friedegg796/TutorialStructures?tab=readme-ov-file"> Github </a> you install the zip, unzip it, and then do the above (Unreleased/Latest versions).
+<p>On <a href="https://github.com/friedegg796/TutorialStructures?tab=readme-ov-file"> Github </a>
+you install the zip, unzip it,
+and then do the above (Unreleased/Latest versions).</p>
 
-Or look for the named zip file in [releases](https://github.com/friedegg796/Muslimcraft-Lite/releases).
+<p>Or look for the named zip file in [releases](https://github.com/friedegg796/Muslimcraft-Lite/releases).</p>
 
     </div>
     </>
